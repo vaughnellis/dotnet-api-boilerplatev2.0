7510338f523fcc68f333439f7549ca7ca6aa0df7
Vaughn Ellis Ramos <vaughnelliseramos@gmail.com>
2023-02-03 14:34:22 +1100

    Add gitignore

---
+++ DotnetApiBoilerplatev2.0.Core/Models/Common/BaseEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotnetApiBoilerplatev2._0.Core.Models.Common
+{
+    public class BaseEntity
+    {
+
+    }
+}
